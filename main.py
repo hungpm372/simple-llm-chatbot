d9fe4075b9ea26aa129834f57dbcd5b578d77261
hungpm372 <hungpm372@gmail.com>
2024-06-26 22:52:26 +0700

    Add chat history store and stream message response

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
+import time
 
 from dotenv import load_dotenv
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
 
 load_dotenv(dotenv_path=".env.dev")
 
+store: dict = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
 model = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-result = model.invoke(
-    [
-        HumanMessage(content="Hi! I'm Hung"),
-        AIMessage(content="Hello Hung! How can I assist you today?"),
-        HumanMessage(content="What's my name?"),
-    ]
-)
+with_message_history = RunnableWithMessageHistory(model, get_session_history)
 
-print(result)
+config = {"configurable": {"session_id": "1"}}
+
+while True:
+    user_input = input("You: ")
+    print(f"Assistant: ", end="")
+    for r in with_message_history.stream(
+            [HumanMessage(content=user_input)],
+            config=config,
+    ):
+        print(r.content, end="")
+        time.sleep(0.05)
+
+    print()
